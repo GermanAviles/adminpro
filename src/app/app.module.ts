6ba200bee4944148460b36e5474e78ab2451cb49
German Aviles <german.aviles06@gmail.com>
2018-12-04 18:44:09 -0500

    login y 404 component creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule
