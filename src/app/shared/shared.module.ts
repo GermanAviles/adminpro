06a3b3190f14e04e987a7f0af2fd9182989fa4e9
German Aviles <german.aviles06@gmail.com>
2018-12-05 10:50:32 -0500

    modulo shared y pages creados y app.module actualizado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+/** Componentes */
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ]
+
+})
+
+export class SharedModule {}
